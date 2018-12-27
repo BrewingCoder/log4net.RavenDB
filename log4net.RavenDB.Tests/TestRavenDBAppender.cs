afd171b81e210e391acced2d51e64dcb25df8cf0
BrewingCoder <scott@gscottsingleton.com>
2018-12-27 00:37:31 -0500

    added unit Tests

---
+++ log4net.RavenDB.Tests/TestRavenDBAppender.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using log4net.Config;
+
+namespace log4net.RavenDB.Tests
+{
+    [TestClass]
+    public class TestRavenDBAppender
+    {
+        private static readonly log4net.ILog log =log4net.LogManager.GetLogger(typeof(TestRavenDBAppender));
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+            XmlConfigurator.Configure();
+
+            log.Info("Test");
+        }
+    }
+}
